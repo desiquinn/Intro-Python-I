94081e8396b8803e9cab4043f9dc65b3bc6442e6
Desiree Morris <desiquinncompany@gmail.com>
2020-01-14 16:08:06 -0500

    completed cal.py

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,27 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+curr_month = datetime.today().month
+curr_year = datetime.today().year
+
+def print_cal(month = curr_month, year = curr_year):
+    if 0 < month < 13:
+        print(calendar.month(year, month, 2, 1))
+    else:
+      print('The month must be a number between 1 and 12')
+
+cal_input = input("~~> ").split(",")
+
+print(cal_input)
+
+if len(cal_input) == 0:
+    print_cal()
+elif len(cal_input) == 1:
+    print_cal(int(cal_input[0]))
+elif len(cal_input) == 2:
+    print_cal(int(cal_input[0]), int(cal_input[1]))
+else:
+    print("input must include a month(1-12), or a month and year (2, 1986)")
+
+# to make this better I would validate the year as well and return an error
